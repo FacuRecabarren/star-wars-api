4aa6a49ed021df9cee155e1438ef5cc50a21c21e
Facundo Recabarren <facundorecabarren10@gmail.com>
2024-05-13 15:47:54 -0300

    add(-): create footer component

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='h-[10rem] bg-[#0d0d0d] flex justify-center items-center relative'>
+        <div className='flex justify-center items-center gap-5 text-4xl text-white'>
+            <a href='https://github.com/FacuRecabarren' target='_blank' className="icon-[mdi--github] hover:text-[#ffe81f] duration-300">GitHub</a>
+            <a href='https://www.linkedin.com/in/facundorecabarren/' target='_blank' className="icon-[mdi--linkedin] hover:text-[#ffe81f] duration-300">LinkedIn</a>
+            <a href='https://facureca.vercel.app/' target='_blank' className="icon-[bytesize--portfolio] hover:text-[#ffe81f] duration-300">Portfolio</a>
+        </div>
+        <p className='absolute bottom-4 text-gray-600'>© 2024 Facundo Recabarren</p>
+    </div>
+  );
+};
+
+export default Footer;
